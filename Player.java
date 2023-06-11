5479795f1a229fcb2229cc12be52e94ece9d01bd
Tejas <87278292+TejasK28@users.noreply.github.com>
2023-06-11 04:01:29 +0000

    Planning

--- Player.java
+++ Player.java
@@ -9,6 +9,10 @@ public class Player
     Property property_owned;
     Space currSpace;
 
+    /*
+     * Constructor for creating a new Player instance
+     * Parameters: name, bank reference, community chest reference, chance reference, and current space 
+     */
     public Player(String name, Bank bank, Deck<Chance> chance_deck, Deck<CommunityChest> communinty_chest_deck, Property property_owned, Space currSpace)
     {
         this.name = name;
@@ -20,23 +24,35 @@ public class Player
         this.currSpace = currSpace;
     }
 
-
-
-
-
-
     /*
-     * Method
+     * Method   
      * Parameters: property to buy
      * Returns: true if property is succesfully acquired
      */
     public boolean buy(Space property)
     {
+
     }
 
+    /*
+     * Method
+     * Parameters: property to auction
+     * Returns: true if property is succesfully auctioned
+     */
+    public boolean auction(Space property)
+    {
 
+    }
 
-    
+    /*
+     * Method
+     * Parameters: none
+     * Returns: true if player lands 3 doubles and ends up in jail
+     */
+    public boolean goToJail()
+    {
+
+    }
 
     
 
