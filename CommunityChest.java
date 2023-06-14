21b758867568cd7779eea1debdded7af7a09a7aa
Tejas <87278292+TejasK28@users.noreply.github.com>
2023-06-14 04:12:54 +0000

    Community Chest Update

--- CommunityChest.java
+++ CommunityChest.java
@@ -1,5 +1,13 @@
 public class CommunityChest extends Card
 {
+    String prompt;
+    CommunityChestType type;
 
+    //Constructor for community chest card
+    public CommunityChest(String prompt, CommunityChestType type)
+    {
+        this.prompt = prompt;
+        this.type = type;
+    }
     
 }
